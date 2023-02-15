840b03727d5c9f9f03b9381b3abcb60458f6e26d
FlorezZ1025 <92589772+FlorezZ1025@users.noreply.github.com>
2023-02-15 14:53:22 -0500

    Update Matriz.py

--- Matriz.py
+++ Matriz.py
@@ -8,4 +8,22 @@ def SumatoriaMatriz(Matriz): #n
     return sum(sum(fila) for fila in Matriz) #n
 n = int(input("Ingrese el valor n: ")) #n
 m = int(input("Ingrese el valor m: ")) #n
+
+def Mostrar_ZigZag(matriz,flag=True,n=0):
+    for j in range(len(matriz[0])):
+        if flag:
+            for i in range(len(matriz)):
+                print(matriz[i][n])
+            flag= False
+        else:
+            s=len(matriz)
+            for i in range(len(matriz)):
+                print(matriz[s-1][n])
+                s-=1
+            flag=True
+        n+=1
+
 print("La sumatoria es: ", SumatoriaMatriz(MostrarMatriz(CrearMatriz(n,m)))) #^2
+
+matriz = MostrarMatriz(CrearMatriz(n,m))
+Mostrar_ZigZag(matriz)
