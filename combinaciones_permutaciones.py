eba9bca08c25e0f7f1db2ab9ff2872c70e0c9e2c
Dlassoc <99360471+Dlassoc@users.noreply.github.com>
2023-03-02 10:28:08 -0500

    Update combinaciones_permutaciones.py

--- combinaciones_permutaciones.py
+++ combinaciones_permutaciones.py
@@ -1,6 +1,14 @@
 def es_permutacion_o_combinacion():
     pregunta = input("Es permutación o combinación")
     if pregunta == "permutación":
-        pass
+        permutacion()
     else:
         pass 
+def permutacion (pregunta):
+    orden = input("¿Importa el orden? si/no")
+    orden.lower()
+    if orden == "si":
+        valor_n = input("Ingrese valores en n")      
+        valor_m = input("Ingrese valores en m")
+        
+
