7cfb957d4c82b4086cf228c7bb188bde02a1649f
Dlassoc <99360471+Dlassoc@users.noreply.github.com>
2023-03-03 15:42:09 -0500

    Pues por acá estamos haciendo la versión oficial

--- combinaciones_permutaciones.py
+++ combinaciones_permutaciones.py
@@ -1,49 +1,40 @@
 from math import factorial
 
-m = int(input("Ingrese cantidad total de elementos"))
-n = int(input("Ingrese cantidad de probabilides"))
-def orden():
-    print("""0.Si
-                1.No""")
-    pregunta = int(input("Importa el orden"))
-    if pregunta == 0:
-        orden_si(pregunta)
-    elif pregunta == 1:
-        pass 
+def combinacion_con_repeticion(m,n):
+    solucion = factorial((m + n) - 1) / (factorial(n) * (factorial(m - 1)))
+    return solucion
+
+def combinacion_ordinaria(m,n):
+
+    solucion = factorial(m)/(factorial(n) * (factorial(m - n)))
+    return solucion
+
+def permutacion_con_repeticion(lista_numeros):
+    denominador = 1
+    P = sum(lista_numeros)
+    for numero in lista_numeros:
+        actual = factorial(numero)
+        denominador = denominador * actual
+    solucion = factorial(P)/denominador
+    return solucion
+
+def permutacion_ordinaria(m):
+    solucion = factorial(m)
+    return 
+def variaciones_con_repeticion(m,n):
+    solucion = m**n
+    return solucion
+def variacion_ordinaria(m,n):
+    solucion = factorial(m)/(factorial(m-n))
+    return solucion
+    
+#print(combinacion_con_repeticion(22,4))
+#print(combinacion_ordinaria(22, 4))
+#lista = [3,2]
+#print(permutacion_con_repeticion(lista))
 
 
-def orden_si (pregunta):
-    entran_todos_los_elementos = int(input("¿Entran todos los elementos?"))
-    print("""0.Si
-                1.No""")
-    if entran_todos_los_elementos == 0:
-        permutaciones()
-    elif entran_todos_los_elementos == 1:
-        variaciones()
 
-def se_repiten_elementos(): #LA IDEA DE ESTE METODO ES EVITAR HACER LAS MISMAS LINEAS DE CODIGO EN EL PROGRAMA, ESTA PREGUNTA SE HARÁ EN PERMUTACION Y VARIACION PERO NO SE SI ESTÁ BIEN
-    si_no = int(input("¿Se repiten los elementos?"))
-    print("""0.Si
-                1.No""")  
-    return si_no
-def permutaciones():
-    a = se_repiten_elementos()
-    if a == 0:
-        # La formula es P_n / a! b! c!, pero no sabemos que es P o al menos santiago y yo :p
-        pass
-    elif a == 1:
-        operacion = m**n
-        print(f"El valore es {operacion}")
-
-def variaciones():
-    a = se_repiten_elementos()
-    if a == 0:
-        solucion = m**n
-        print(f"El valor es {solucion}")
-        
-    elif a == 1:
-        solucion = factorial(m) / factorial(m-n) 
-        print(f"El valor es {solucion}")
 
  
 
