6115a2f976c4f5f87a2f1dda1263473e10918005
multihexagon <98978765+multihexagon@users.noreply.github.com>
2023-05-03 15:33:28 -0500

    Update main.py

--- Taller #5/main.py
+++ Taller #5/main.py
@@ -14,3 +14,19 @@ def funcion_taller2(A, B):
     return resultado
 
 print(funcion_taller2(lista1,lista2))
+
+
+def minimo(numero):
+    pasos = 1
+    numerote = 1
+    while((numerote *2)<=numero):
+            pasos = pasos + 1
+            numerote = numerote *2 
+            print('pasos ',pasos)
+            print('numerote ', numerote)
+    while((numerote + 1 )<=numero):
+        numerote = numerote + 1
+        pasos = pasos + 1
+        
+    return pasos
+print(minimo(7))    
