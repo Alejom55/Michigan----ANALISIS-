d69fbf901b5e257863e4237dbf0d5b93727ce76f
FlorezZ1025 <92589772+FlorezZ1025@users.noreply.github.com>
2023-05-24 22:32:51 -0500

    Update bonificacion.py

--- bonificacion.py
+++ bonificacion.py
@@ -1,51 +1,29 @@
-def matriz(n):
-    m = []
-    for i in range(n):
-        m.append([])
-        for j in range(n):
-            m[i].append(0)
-    return m
+def insertar_reina(fila,n, reinas,sols=[]):
+  if fila == n:
+    return 1
+  else:
+    soluciones = 0
+    for columna in range(n):
+      if pos_valida(fila,columna,reinas):
+        reinas[fila]=columna
+        soluciones += insertar_reina(fila+1,n,reinas,sols)
+    return soluciones
+
+def pos_valida(fila,columna,reinas):
+  for f in range(fila):
+    if columna == reinas[f]:
+      return False
+
+    elif abs(columna - reinas[f]) == abs(fila- f): 
+      return False
+  return True
+
+def n_reinas(n):
+  reinas = []
+  for i in range(n):
+    reinas.append("")
+  fila = 0  
+  return insertar_reina(fila, n, reinas)
 
 
-def posiciones(matriz):
-    l = []
-    for i in range(len(matriz)):
-        for j in range(len(matriz)):
-            l.append((i,j))
-    return l    
-
-def insertarReina(lista: list, matriz: list):    
-    while True:
-        print(f"lista inicial: {lista}")
-        i = int(input("Pon la posicion i "))
-        j = int(input("Pon la posicion j "))
-        if (i,j) not in lista:
-            raise "Seleccione uno que si este en la lista"
-        else:
-            matriz[i][j] = "*"
-            lista.remove((i,j))
-            for posicion in range(len(matriz)):
-                if (i,j+posicion) in lista:
-                    lista.remove((i,j+posicion))
-                if (i+posicion,j) in lista:
-                    lista.remove((i+posicion,j))        
-                if (i,j-posicion) in lista:
-                    lista.remove((i,j-posicion))
-                if (i-posicion,j) in lista:
-                    lista.remove((i-posicion,j))
-                if (i+posicion,j-posicion) in lista:
-                    lista.remove((i+posicion,j-posicion))
-                if (i-posicion,j+posicion) in lista:
-                    lista.remove((i+posicion,j+posicion))
-                if (i+posicion,j+posicion) in lista:
-                    lista.remove((i+posicion,j+posicion))
-                if (i-posicion,j-posicion) in lista:
-                    lista.remove((i-posicion,j-posicion))
-            print(lista)
-            print(matriz)
-
-
-#n = int(input("Ponga el tamaño de la matriz nxn"))
-matriz = matriz(4)
-print(insertarReina( posiciones(matriz),matriz))
-
+n_reinas(4)
